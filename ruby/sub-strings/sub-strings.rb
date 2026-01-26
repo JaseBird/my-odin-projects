cdce8dd41875eb61965f315b813897b68aa1057e
Jase Bird <37068795+JaseBird@users.noreply.github.com>
2026-01-26 16:26:40 +0000

    feat: add ruby sub-strings project

--- ruby/sub-strings/sub-strings.rb
+++ ruby/sub-strings/sub-strings.rb
@@ -3,6 +3,25 @@
 # argument. It should return a hash listing each substring (case insensitive)
 # that was found in the original string and how many times it was found.
 
-def substrings(searchWord, validSubstrings)
-  # TODO
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(strings, dictionary)
+  result = Hash.new(0)
+  words = strings.downcase.split(/\W+/)
+
+  dictionary.each do |substring|
+    substring = substring.downcase
+
+    words.each do |w|
+      if w.include?(substring)
+        result[substring] += 1
+      end
+    end
+  end
+  return result
 end
+
+puts substrings("Howdy I am Below you and I want to go, I will be part of something big!", dictionary)
+
+# Next, make sure your method can handle multiple words
+# Recall how to turn strings into arrays and arrays into strings.
