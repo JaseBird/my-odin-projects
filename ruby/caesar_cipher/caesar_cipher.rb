690592be85f2071ca53f800b36a48853c61d0fdf
Jase Bird <37068795+JaseBird@users.noreply.github.com>
2026-01-26 15:08:26 +0000

    add ruby caesar cipher project

--- ruby/caesar_cipher/caesar_cipher.rb
+++ ruby/caesar_cipher/caesar_cipher.rb
@@ -4,21 +4,17 @@ def caesar_cipher(string, offset)
   # Don’t forget to keep the same case
   # 65 = A => 90 => Z, 97 = a => 122 = z
 
-  puts "Orginal string: " + string
-
   # if string contains data
   unless string.empty?
     string.each_char.map do |char|
       case char
       when 'a'..'z'
-        ((char.ord - 'a'.ord + offset) % 26 + 'a'.ord).chr
+        ((char.ord - 'a'.ord - offset) % 26 + 'a'.ord).chr
       when 'A'..'Z'
-        ((char.ord - 'A'.ord + offset) % 26 + 'A'.ord).chr
+        ((char.ord - 'A'.ord - offset) % 26 + 'A'.ord).chr
       else
         char
       end
     end.join
   end
 end
-
-puts "Encription: " + caesar_cipher("azAZ !?", 5)
