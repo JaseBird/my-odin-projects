a31c0e361fd50f7c174240aa9ac6c4059d433ad1
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2026-08-24 11:15:11 +0100

    feat: add current player valid position selection

--- ruby/tic-tac-toe/game.rb
+++ ruby/tic-tac-toe/game.rb
@@ -7,11 +7,13 @@ class Game
     @board = GameBoard.new
     @player_one = Player.new(get_player_name(1), "x")
     @player_two = Player.new(get_player_name(2), "o")
+    @current_player = @player_one
   end
 
   def get_player_name(player_number)
     puts "Player #{player_number}, enter your name: "
     name = gets.chomp
+    return name
   end
 
   def display_header_message
@@ -28,24 +30,42 @@ class Game
     puts "-----------------------------------------"
   end
 
-  def get_player_selection(player)
-    puts ""
+  def get_valid_position(current_player)
+    valid_move = false
+    puts "#{current_player.name} can you please select a position:"
+    
+    while !valid_move
+      selection = gets.chomp.to_i
+
+      if @board.space_available?(selection)
+        valid_move = true
+      else
+        puts "Selection not valid, please try again:"
+      end
+    end
+    return selection
   end
 
-   def start()
+  def start()
     display_start_game_message
-
-    # 1. Display the board
-    # 2. Tell current player its their turn
-    # 3. Ask them for a position
-    # 4. Check weather the position is valid
-    # 5. If invalid -> ask again
-    # 6. If valid -> place thier place
-    # 7. Check whether they've won
-    # 8. If not -> switch player
-    # 9. Repeat for 9 turns
+    
+    # ask current player for a position and return only when valid
+    @board.place_piece(get_valid_position(@current_player), @current_player.piece)
+    @board.display
   end
 end
 
 game = Game.new()
 game.start
+
+
+#GAME TURN LOGIC
+  # 1. Display the board
+  # 2. Tell current player its their turn
+  # 3. Ask them for a position
+  # 4. Check weather the position is valid
+  # 5. If invalid -> ask again
+  # 6. If valid -> place thier place
+  # 7. Check whether they've won
+  # 8. If not -> switch player
+  # 9. Repeat for 9 turns
