180df0bd38ded8eaec680d15090dd7db73f8b155
Jason Bird <37068795+JaseBird@users.noreply.github.com>
2026-08-24 14:26:44 +0100

    feat: add computer function to create secret code

--- ruby/mastermind/lib/computer.rb
+++ ruby/mastermind/lib/computer.rb
@@ -1,3 +1,12 @@
 class Computer
-  
+  def create_code()
+    available_colours = ["Blue", "Green", "Purple", "Red", "White", "Yellow"]
+    code = []
+
+    4.times do |i|
+      code[i] = available_colours[rand(0..5)]
+    end
+    
+    return code
+  end
 end
--- ruby/mastermind/lib/game.rb
+++ ruby/mastermind/lib/game.rb
@@ -1,7 +1,8 @@
-require_relative 'code'
+require_relative 'secret_code'
 require_relative 'computer'
 require_relative 'player'
 
 class Game
-  
+  computer = Computer.new
+  puts computer.create_code
 end
